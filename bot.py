811a860fcb754916f232ebcf8be001750a50f878
oget565 <149737437+Oget565@users.noreply.github.com>
2025-11-19 19:25:58 -0500

    func: owner check

--- bot.py
+++ bot.py
@@ -1,18 +1,31 @@
+from dotenv import load_dotenv
 import asyncio
-import sys
+import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command, BaseFilter
+
+load_dotenv()
 
 dp = Dispatcher()
+bot = Bot(token=os.getenv("BOT_TOKEN"))
 
-def main():
-    ...
+owner_id = int(os.getenv("OWNER_ID"))
 
+class IsOwner(BaseFilter):
+    async def __call__(self, message: types.Message) -> bool:
+        return message.from_user.id == owner_id
 
+async def main():
+    @dp.message(Command('start'), IsOwner())
+    async def welcome(message: types.Message):
+        await message.answer("Hello!")
+    
+    @dp.message(~IsOwner())
+    async def unauthorized(message: types.Message):
+        await message.answer("You are not the owner!")
 
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    main()
-
-
-
+    asyncio.run(main())
