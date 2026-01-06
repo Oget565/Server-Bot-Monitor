e2efe554ee179f23a59039982d9ef54aa4216d6a
Oget565 <149737437+Oget565@users.noreply.github.com>
2026-01-06 12:07:53 -0500

    Read and write function

--- app/settings/read_write_settings.py
+++ app/settings/read_write_settings.py
@@ -1,5 +1,18 @@
-def read_settings(item):
-    ...
+import json
 
-def read_settings(item):
-    ...
+def read_settings(val):
+    with open('settings.json', 'r') as f:
+        data = json.load(f)
+        print(data[val])
+
+def write_settings(item, val):
+    with open('settings.json', 'r') as f:
+        data = json.load(f)
+
+    data[item] = val
+    
+    with open('settings.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+read_settings("timezone")
+write_settings("update_interval", 5)
